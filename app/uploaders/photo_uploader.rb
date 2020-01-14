1e6339d7b5cf42c2d3f13d4e586da7ab815d4203
taras-brodovskiy <toss188@gmail.com>
2020-01-14 10:55:03 +0300

    gitignore

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,9 +2,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [100, 100]
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
